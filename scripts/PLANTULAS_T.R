33e2808ef7af44308cca2726d72d6e7f7f871bcb
AndreaGzz9 <andybanuelas91@gmail.com>
2023-05-11 09:38:53 -0600

    terminado boxplots

--- scripts/PLANTULAS_T.R
+++ scripts/PLANTULAS_T.R
@@ -19,3 +19,27 @@ t.test(plantulas$IE ~ plantulas$Tratamiento, var.equal=T)
 mean(plantulas$IE)
 t.test(plantulas$IE, mu = 0.90)
 t.test(plantulas$IE, mu =0.85)
+
+#importar archivo
+semilla <-read.csv("mainproduccion.csv", header = TRUE)
+
+
+#tiempo 
+semilla$Tiempo <-as.factor(semilla$Tiempo)
+
+boxplot(semilla$Tiempo , semilla$Kgsem)
+
+boxplot(semilla$Tiempo , semilla$BioRama)
+
+boxplot(semilla$Tiempo , semilla$Germ)
+
+#prueba de t
+t.test(semilla$Kgsem ~ semilla$Tiempo, paired = TRUE)
+
+t.test(semilla$Germ ~ semilla$Tiempo, paired = TRUE)
+
+boxplot(semilla$Kgsem)
+
+t.test(semilla$H6 ~ semilla$Tiempo, paired = TRUE)
+
+t.test(semilla$BioRama ~ semilla$Tiempo, paired = TRUE)
